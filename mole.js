3408dfb3bd881cf3aa4db6a9078370d42dc63a19
animesh pradhan <animesh22junepradhan@gmail.com>
2023-07-08 13:16:16 +0530

    updated code

--- mole.js
+++ mole.js
@@ -2,9 +2,21 @@ let currMoleTile;
 let currPlantTile;
 let score = 0;
 let gameOver = false;
+let timeLimit = 60; // Time limit in seconds
+let minimumScore = 250; // Minimum score required to win
+let timerInterval;
 
 window.onload = function() {
+    document.getElementById("startButton").addEventListener("click", startGame);
+}
+
+function startGame() {
     setGame();
+
+    startTimer(); // Start the timer
+    setInterval(setMole, 600);
+    setInterval(setPlant, 700);
+    document.getElementById("startButton").disabled = true;
 }
 
 function setGame() {
@@ -16,8 +28,8 @@ function setGame() {
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
-    setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+    //setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
+    //setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
 }
 
 function getRandomTile() {
@@ -43,6 +55,7 @@ function setMole() {
     currMoleTile = document.getElementById(num);   //represents the current tile where the mole will appear.
     currMoleTile.appendChild(mole);                //appends the mole image element as a child to the currMoleTile, 
                                                    //placing the mole image on the game board.
+    
 }
 
 function setPlant() {
@@ -70,9 +83,38 @@ function selectTile() {
     if (this == currMoleTile) {      //checks if the clicked tile is the same as the currMoleTile, indicating that the player clicked on the tile with the mole.
         score += 10;
         document.getElementById("score").innerText = score.toString(); //update score html
+
+        if (score >= minimumScore) {
+            document.getElementById("score").innerText = "YOU WIN: " + score.toString();
+            clearInterval(timerInterval);
+            gameOver = true;
+        }
     }
     else if (this == currPlantTile) {    //If the clicked tile is the same as the currPlantTile, indicating that the player clicked on the tile with the plant.
         document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
+        clearInterval(timerInterval);
         gameOver = true;
     }
+}
+
+function startTimer() {
+    let timerDisplay = document.getElementById("timer");
+
+    let remainingTime = timeLimit;
+    timerDisplay.innerText = remainingTime.toString();
+
+    timerInterval = setInterval(function() {
+        remainingTime--;
+        timerDisplay.innerText = "Time Remaining: " + remainingTime.toString();
+
+        if (remainingTime <= 0) {
+            clearInterval(timerInterval);
+            if (score >= minimumScore) {
+                document.getElementById("score").innerText = "YOU WIN: " + score.toString();
+            } else {
+                document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+            }
+            gameOver = true;
+        }
+    }, 1000);
 }
